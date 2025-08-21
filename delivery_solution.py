58749a0a4346456122a17ecdef13af4d114ad88e
Anastasia Babkina <nastya.babkina.1997@mail.ru>
2025-08-21 16:07:40 +0500

    The delivery service solution

--- delivery_solution.py
+++ delivery_solution.py
@@ -1,18 +1,15 @@
-# 141423521 - ID посылки
+# 141427376 - ID посылки
 import sys
 
 
-def get_number_of_platforms(data: list, limit: int) -> int:
+def get_number_of_platforms(data_in: list, limit: int) -> int:
     """The delivery service: main function."""
+    data = sorted(data_in[:])
     platforms: int = 0
     left_pointer: int = 0
     right_pointer: int = len(data) - 1
     while left_pointer <= right_pointer:
-        total: int = None
-        if left_pointer != right_pointer:
-            total = data[left_pointer] + data[right_pointer]
-        else:
-            total = data[left_pointer]
+        total: int = data[left_pointer] + data[right_pointer]
         platforms += 1
         right_pointer -= 1
         if total <= limit:
@@ -21,7 +18,7 @@ def get_number_of_platforms(data: list, limit: int) -> int:
 
 
 if __name__ == '__main__':
-    data: list = [int(item) for item in sys.stdin.readline().rstrip().split()]
+    data_in: list = [int(item) for item in
+                     sys.stdin.readline().rstrip().split()]
     limit: int = int(sys.stdin.readline().rstrip())
-    data.sort()
-    print(get_number_of_platforms(data, limit))
+    print(get_number_of_platforms(data_in, limit))
