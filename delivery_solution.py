704b0c5aeaf6ca87fb7284d34f97da47d173b325
Anastasia Babkina <nastya.babkina.1997@mail.ru>
2025-08-21 13:49:58 +0500

    the delivery service solution

--- delivery_solution.py
+++ delivery_solution.py
@@ -1,40 +1,27 @@
-# 141325975 - ID посылки
+# 141423521 - ID посылки
 import sys
 
 
 def get_number_of_platforms(data: list, limit: int) -> int:
     """The delivery service: main function."""
     platforms: int = 0
-    data.sort()
     left_pointer: int = 0
     right_pointer: int = len(data) - 1
-
-    def get_step_left(platforms: int, right_pointer: int) -> tuple:
-        """Takes a step to the left."""
+    while left_pointer <= right_pointer:
+        total: int = None
+        if left_pointer != right_pointer:
+            total = data[left_pointer] + data[right_pointer]
+        else:
+            total = data[left_pointer]
         platforms += 1
         right_pointer -= 1
-        return platforms, right_pointer
-
-    def get_step_right(left_pointer: int) -> int:
-        """Takes a step to the right."""
-        left_pointer += 1
-        return left_pointer
-
-    while left_pointer <= right_pointer:
-        total: int = data[left_pointer] + data[right_pointer]
         if total <= limit:
-            platforms, right_pointer = get_step_left(platforms, right_pointer)
-            left_pointer = get_step_right(left_pointer)
-        elif total > limit:
-            platforms, right_pointer = get_step_left(platforms, right_pointer)
-        else:
-            left_pointer = get_step_right(left_pointer)
-    if left_pointer == right_pointer:
-        platforms += 1
+            left_pointer += 1
     return platforms
 
 
 if __name__ == '__main__':
     data: list = [int(item) for item in sys.stdin.readline().rstrip().split()]
     limit: int = int(sys.stdin.readline().rstrip())
+    data.sort()
     print(get_number_of_platforms(data, limit))
